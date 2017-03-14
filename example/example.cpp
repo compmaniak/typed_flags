f95e30a0f56f6978b28d00e2f014364c2e215955
compmaniak <compmaniak@mail.ru>
2017-03-14 22:56:25 +0300

    Make example

--- example/example.cpp
+++ example/example.cpp
@@ -1,4 +1,51 @@
+#include "../include/typed_flags.hpp"
+#include <cassert>
+
+// at first declare some types
+class eats_meat;
+class eats_grass;
+class has_tail;
+class build_spaceships;
+
+// then bind types to flag identifiers
+typedef typed_flags<eats_meat, eats_grass, has_tail> animal;
+typedef typed_flags<eats_meat, eats_grass, build_spaceships> engineer;
+
 int main()
 {
-    return 0;
+    // unlike std::bitset typed_flags allocates less memory
+    assert( sizeof(animal) == 1 );
+    
+    // create flags from scratch
+    animal wolf;
+    wolf.set<eats_grass>(false);
+    wolf.set<eats_meat, has_tail>();
+    wolf.set(flag<has_tail>{1}, flag<eats_meat>{1});
+    
+    // create flags with flexible human-readable constructor
+    wolf = animal{flag<has_tail>{1}, flag<eats_meat>{1}, flag<eats_grass>{0}};
+    
+    // test each flag separately
+    assert( (wolf.test<eats_meat>() && wolf.test<has_tail>()) );
+    
+    // test group off flags in one call
+    assert( (wolf.all<eats_meat, has_tail>()) );
+    assert( (wolf.any<eats_meat, has_tail, eats_grass>()) );
+    assert( (wolf.none<eats_grass>()) );
+    
+    // like std::bitset create flags from integers or strings 
+    // and convert vice versa
+    auto a1 = animal{3};
+    auto a2 = animal{"101"};
+    assert( a1.to_integral<int>() == 3 );
+    assert( a2.to_string() == "101" );
+    
+    // there are bitwise member and non-member functions
+    auto a3 = wolf;
+    a3 &= animal{"001"};
+    assert( a3 == animal{flag<eats_meat>{1}} );
+    assert( (a3 | animal{2}) == animal{3} );
+    
+    //wolf.set<build_spaceships>(); // compile error!
+    //wolf = engineer{1}; // compile error!
 }
