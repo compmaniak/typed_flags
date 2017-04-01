7774c41ef6a1e6c87ed3c189dada861d70cacb1b
tux <compmaniak@mail.ru>
2017-04-01 19:17:24 +0300

    More compact folding

--- include/detail/meta17.hpp
+++ include/detail/meta17.hpp
@@ -13,19 +13,9 @@
 namespace tfl::detail
 {
 
-//
-// Entity representing one flag with its type and index
-//
 template<typename T, size_t N>
-struct name_index
-{
-    typedef T type;
-    static constexpr size_t index = N;
-};
+struct name_index;
 
-//
-// Sequence of name_index entities
-//
 template<typename... Args>
 struct name_index_sequence;
 
@@ -38,53 +28,33 @@ struct to_name_index_sequence<std::index_sequence<I...>, Args...>
     typedef name_index_sequence<name_index<Args, I>...> type;
 };
 
-//
-// Helper class counting occurrences of T in Args
-//
 template<typename T, typename... Args>
 struct counter
 {
     static constexpr size_t value = (0 + ... + std::is_same<T, Args>::value);
 };
 
-//
-// Helper class checking that every type is unique.
-// Every type must appear in sequence only once.
-//
 template<typename... Args>
 struct is_unique
 {
     static constexpr bool value = (... && (counter<Args, Args...>::value == 1));
 };
 
+enum index: size_t {};
+
+constexpr index operator << (index lhs, index rhs)
+{
+    return lhs < rhs ? lhs : rhs;
+}
+
 template<typename T, typename U>
 struct index_getter;
 
-struct index
+template<typename T, size_t... I, typename... Args>
+struct index_getter<T, name_index_sequence<name_index<Args, I>...>>
 {
-    size_t value = -1;
-    
-    index() = default;
-    
-    constexpr explicit index(size_t v): value(v) {}
-    
-    constexpr explicit operator size_t() const { return value; }
-};
-
-constexpr index operator << (index const& lhs, index const& rhs)
-{
-    return lhs.value < rhs.value ? lhs : rhs;
-}
-
-//
-// Find type by matching it with each element in the list
-//
-template<typename T, typename... Args>
-struct index_getter<T, name_index_sequence<Args...>>
-{
-    static constexpr size_t value = static_cast<size_t>(
-        (index{} << ... << (std::is_same<T, typename Args::type>
-            ::value ? index{Args::index} : index{})));
+    static constexpr size_t value = (index(-1) << ... << (
+        std::is_same<T, Args>::value ? index(I) : index(-1)));
 };
 
 } // namespace tfl::detail
