72f71f002994d2d8898d1a1f966638d053feb2c8
tux <compmaniak@mail.ru>
2017-05-22 04:08:00 +0300

    Simplify facet14

--- include/detail/facet14.hpp
+++ include/detail/facet14.hpp
@@ -28,7 +28,7 @@ public:
     template<typename... T>
     bool none() const noexcept {
         bool r = true;
-        int _[] = {0, (r = r && !this_().template test<T>(), 0)...};
+        auto _ = {0, (r = r && !this_().template test<T>(), 0)...};
         (void)_;
         return r;
     }
@@ -36,38 +36,38 @@ public:
     template<typename... T>
     bool all() const noexcept {
         bool r = true;
-        int _[] = {0, (r = r &&  this_().template test<T>(), 0)...};
+        auto _ = {0, (r = r &&  this_().template test<T>(), 0)...};
         (void)_;
         return r;
     }
 
     template<typename... T>
     void set(bool value = true) noexcept {
-        int _[] = {0, (this_().set_bit(D::template index<T>(), value), 0)...};
+        auto _ = {0, (this_().set_bit(D::template index<T>(), value), 0)...};
         (void)_;
     }
 
     template<typename... T>
     void set(flag<T>... flags) noexcept {
-        int _[] = {0, (this_().set_bit(D::template index<T>(), flags), 0)...};
+        auto _ = {0, (this_().set_bit(D::template index<T>(), flags), 0)...};
         (void)_;
     }
 
     template<typename... T>
     void get(flag<T>&... flags) const noexcept {
-        int _[] = {0, (flags = this_().template test<T>(), 0)...};
+        auto _ = {0, (flags = this_().template test<T>(), 0)...};
         (void)_;
     }
 
     template<typename... T>
     void reset() noexcept {
-        int _[] = {0, (this_().set_bit(D::template index<T>(), false), 0)...};
+        auto _ = {0, (this_().set_bit(D::template index<T>(), false), 0)...};
         (void)_;
     }
 
     template<typename... T>
     void flip() noexcept {
-        int _[] = {0, (this_().set_bit(D::template index<T>(), !this_().template test<T>()), 0)...};
+        auto _ = {0, (this_().set_bit(D::template index<T>(), !this_().template test<T>()), 0)...};
         (void)_;
     }
 };
